f1ad684915e92e30fe42b03fd7e3743cbdedb3b3
Matthew Joseph <matthew.j.joe@gmail.com>
2021-02-24 22:41:53 -0500

    adding in node for HTTP request to bridge

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
+requrie('dotenv').config
+const axios = requrie('axios'); 
+
 let backBtn = document.getElementById("backBtn");
 let view1 = document.getElementById("view1");
 let view2 = document.getElementById("view2");
 let view3 = document.getElementById("view3");
 
-backBtn.addEventListener("click", backHome);
+//hue variables
+let HUE_BRIDGE_IP = '10.0.0.22';
+let HUE_USERNAME = 'jRGr7F1n41tVAC7sfU6PePPSsy0L02ShzBerTtCp';
 
+$("input").focus(function(){
+    document.activeElement.blur();
+});
 
 document.querySelectorAll('#lightButton').forEach(item => {
     item.addEventListener('click', event => {
@@ -12,11 +20,29 @@ document.querySelectorAll('#lightButton').forEach(item => {
     })
   })
 
-function backHome(){
-    window.location.href = 'https://codecreate.tech/home'
-}
+const controlLights = async (lightID, on) => {
+    try {
+        return await axios.put(
+            `http://${process.env.HUE_BRIDGE_IP}/api/${process.env.HUE_USERNAME}/lights/${lightID}/state`,
+            {on});
+    }catch(err){
+        console.log(error);
+    }
+
+
+    const url = `http://${process.env.HUE_BRIDGE_IP}/api/${process.env.HUE_USERNAME}/lights/${lightID}/state`,
+    { on };
+    try {
+        return await axios.put(url, {
+            on: true,
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
 
 function displayTimer(lightClass){
+    $("#section1_view1").addClass('fullscreen');
     view1.style.display = "none";
     view2.style.display = "block";
 
@@ -39,8 +65,11 @@ function displayTimer(lightClass){
         break;
     }
 
-    document.getElementById("timerLightContainer").innerHTML = '<div class="' + lightClass +'" id="lightButton"> <span id="lightBulb">💡</span><span>'+ userLight +'</span></div>'
+    document.getElementById("timerLightContainer").innerHTML = '<div class="' + lightClass +'" id="lightButton_step2"> <span id="lightBulb">💡</span><span>'+ userLight +'</span></div>'
     $('#timePicker').timepicker();
 }
 
-
+function submitTimer(){
+    controlLights(3, true);
+    alert("Timer started!");
+}
